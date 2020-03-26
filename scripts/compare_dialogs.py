711e0f54fd77af08f7b59959df9ca234a71a12b7
Xavier Verges <xavier.verges@es.ibm.com>
2020-03-26 07:44:52 +0100

    Make compare dialogs output friendlier

--- scripts/compare_dialogs.py
+++ scripts/compare_dialogs.py
@@ -18,6 +18,7 @@ import json
 import logging
 import os
 import os.path
+import pprint
 import sys
 
 from deepdiff import DeepDiff
@@ -61,10 +62,40 @@ def main(argv):
     with open(outputpath) as g:
         dialogOutputUnsorted = json.load(g)
 
-    result = DeepDiff(dialogInputUnsorted, dialogOutputUnsorted, ignore_order=True).json
-    logger.debug("result: %s", json.dumps(result, indent=4))
+    try:
+        if not ((isinstance(dialogInputUnsorted, list) and isinstance(dialogOutputUnsorted, list)) or \
+                (isinstance(dialogInputUnsorted, dict) and isinstance(dialogOutputUnsorted, dict))):
+            # go for DeepDiff
+            raise TypeError
+        equal = True
+        for nodeIn in dialogInputUnsorted:
+            matchingOut = [node for node in dialogOutputUnsorted if node['dialog_node'] == nodeIn['dialog_node']]
+            if not len(matchingOut):
+                equal = False
+                logger.debug('missing output node ' + nodeIn['dialog_node'])
+            elif len(matchingOut) != 1:
+                equal = False
+                logger.debug('extra output nodes for ' + nodeIn['dialog_node'])
+            else:
+                result = DeepDiff(nodeIn, matchingOut[0], ignore_order=True)
+                if result:
+                    equal = False
+                    logger.debug(nodeIn['dialog_node'] + ' ' + pprint.pformat(result))
 
-    if result == '{}':
+            for node in matchingOut:
+                dialogOutputUnsorted.remove(node)
+
+        if len(dialogOutputUnsorted):
+            for node in dialogOutputUnsorted:
+                equal = False
+                logger.debug('unmatched output node ' + node['dialog_node'])
+    except:
+        # For non-lists/dictionaries or stuff without 'dialog_node' key
+        result = DeepDiff(dialogInputUnsorted, dialogOutputUnsorted, ignore_order=True).json
+        logger.debug("result: %s", json.dumps(result, indent=4))
+        equal = result == '{}'
+
+    if equal:
         logger.info("Dialog JSONs are same.")
         exit(0)
     else:
